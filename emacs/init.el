1476baca8f905c97d78038387e2e201f57e4a630
Matteo Cardinaletti <matteo.cardinaletti03@gmail.com>
2025-11-19 22:15:53 +0100

    Adding duplicate-line

--- emacs/init.el
+++ emacs/init.el
@@ -1,13 +1,13 @@
 ; customization of the file where emacs puts its things
-(setq custom-file "~/.config/emacs/custom.el")
+(setq custom-file "~/.emacs.d/custom.el")
 ; loading this configuration
 (load-file custom-file)
 
 ; taken from: https://github.com/rexim/dotfiles
-(load-file "~/.config/emacs/local/rc.el")
+(load-file "~/.emacs.d/local/rc.el")
 
 ; search inside local folder
-(add-to-list 'load-path "~/.config/emacs/local/")
+(add-to-list 'load-path "~/.emacs.d/local/")
 
 (require 'simpc-mode)
 (add-to-list 'auto-mode-alist '("\\.[hc]\\(pp\\)?\\'" . simpc-mode))
@@ -17,7 +17,7 @@
 (tool-bar-mode -1)
 (scroll-bar-mode -1)
 (setq ring-bell-function 'ignore)
-(set-face-attribute 'default nil :height 180)
+(set-face-attribute 'default nil :height 140)
 
 ; ido-mode
 (rc/require 'smex)
@@ -41,3 +41,19 @@
 (rc/require 'move-text)
 (global-set-key (kbd "M-<up>") 'move-text-up)
 (global-set-key (kbd "M-<down>") 'move-text-down)
+
+(defun duplicate-line ()
+ 	"Duplicate current line"
+ 	(interactive)
+	(let ((column (- (point) (point-at-bol)))
+        	(line (let ((s (thing-at-point 'line t)))
+                	(if s (string-remove-suffix "\n" s) ""))))
+    	(move-end-of-line 1)
+    	(newline)
+    	(insert line)
+    	(move-beginning-of-line 1)
+    	(forward-char column))
+)
+
+(global-set-key (kbd "C-,") 'duplicate-line)
+
